2212b4e9c7827a06d047db3f97f67b47a09389c3
sivakarthickraja <sivakarthickraja28@gmail.com>
2024-12-26 03:38:06 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <h1>HI i am karthick</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
